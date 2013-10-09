99888115e51987641db28ea1ebb5da4389fc163b
skxu <skx@berkeley.edu>
2013-10-08 22:23:08 -0700

    rip router initial

--- rip_router.py
+++ rip_router.py
@@ -7,8 +7,31 @@ Create your RIP router in this file.
 class RIPRouter (Entity):
     def __init__(self):
         # Add your code here!
-        pass
+        self.forwardingTable = {}
+        self.routingTable = {}
+        
 
     def handle_rx (self, packet, port):
         # Add your code here!
+        if isinstance(packet, DiscoveryPacket):
+        	discoveryHandler(self, packet, port)
+        elif isinstance(packet,RoutingUpdate):
+        	updateHandler(self, packet, port)
+        else:
+        	if packet.dst in self.forwardingTable:
+        		self.send(packet, self.forwardingTable[packet.dst])
+
+
         raise NotImplementedError
+	def discoveryHandler (self, packet, port):
+		if packet.is_link_up:
+			self.forwardingTable[packet.src] = {}
+			self.forwardingTable[packet.src][packet.src] = (port, 1)
+		elif packet.src in table:
+			del(self.table[packet.src])
+
+
+	def updateHandler (self, packet,port):
+
+
+
